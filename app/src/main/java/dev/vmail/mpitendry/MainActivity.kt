e7c290622c0caa83dcd776077c55c1e22d48c9ad
gizymahmoud-ops <gizymahmoud@gmail.com>
2026-02-15 12:48:09 +0300

    Update MainActivity.kt

--- app/src/main/java/dev/vmail/mpitendry/MainActivity.kt
+++ app/src/main/java/dev/vmail/mpitendry/MainActivity.kt
@@ -3,16 +3,17 @@ package dev.vmail.mpitendry
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import dev.vmail.mpitendry.ui.AppRoot
+import dev.vmail.mpitendry.ui.AppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MaterialTheme {
-                Surface { AppRoot() }
+            AppTheme {
+                AppRoot()
             }
         }
     }
